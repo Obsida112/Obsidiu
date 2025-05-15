9941dce41a7747bfd7dda5f242d9f42b6365d4c7
Obsida112 <BakiHanma112323@proton.me>
2025-05-15 22:50:17 +0200

    Add mockup section to homepage

--- src/components/UI/AnimatedLogo.jsx
+++ src/components/UI/AnimatedLogo.jsx
@@ -51,7 +51,7 @@ const AnimatedLogo = () => {
 
 
   return (
-    <div className="w-[700px] h-[700px] relative flex items-center justify-center">
+    <div className="w-[900px] h-[900px] relative flex items-center justify-center">
       {/* Crystal logo with reveal spin and floating */}
       <motion.div
         initial={{ scale: 0.5, opacity: 0 }}
